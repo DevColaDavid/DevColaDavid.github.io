380c469eaeffbd45b3ec40650f7ea13df20b161a
DevColaDavid <129636069+DevColaDavid@users.noreply.github.com>
2023-04-01 16:08:21 -1000

    Add files via upload

---
+++ Quiz/js/question.js
@@ -0,0 +1,24 @@
+const quiz = [
+    {
+        q:'Example Question1',
+        options:['Correct Answer','Wrong Answer'],
+        answer:0
+    },
+    {
+        q:'Example Question2',
+        options:['Correct Answer','Wrong Answer'],
+        answer:0
+    },
+    {
+        q:'Example Question With Image1',
+        options:['Wrong Answer1','Wrong Answer2','Correct Answer','Wrong Answer3'],
+        answer:2,
+        img: 'img/square.jpg'
+    },
+    {
+        q:'Example Question With Image2',
+        options:['Wrong Answer1','Wrong Answer2','Correct Answer','Wrong Answer3'],
+        answer:2,
+        img: 'img/square.jpg'
+    }
+]
