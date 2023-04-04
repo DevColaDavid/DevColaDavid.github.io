79efbd83e1ecc542b082b6aaf45a62a9f733a0d5
DevColaDavid <129636069+DevColaDavid@users.noreply.github.com>
2023-04-03 14:18:01 -1000

    Update question.js

--- Quiz/js/question.js
+++ Quiz/js/question.js
@@ -1,24 +1,33 @@
 const quiz = [
     {
-        q:'Example Question1',
-        options:['Correct Answer','Wrong Answer'],
+        id:1,
+        q:'1x2x3x4x5x6x7x8x9x1x0x11x12x13x14x15x16x91',
+        options:['0','1','2','3'],
+        answer:0,
+    },
+    {
+        id:2,
+        q:'What is the value of a if (6+1)x3x2+a=a^2? Write the answer as a integer.',
+        options:['7','-2','12','0'],
         answer:0
     },
     {
-        q:'Example Question2',
-        options:['Correct Answer','Wrong Answer'],
-        answer:0
+        id:3,
+        q:'If someone added letters to make the word "David" with 5 letters into "Dwavido," with 7 letters, how much did the number letters increase, in percent?',
+        options:['40','50','60','70'],
+        answer:0,
     },
     {
-        q:'Example Question With Image1',
-        options:['Wrong Answer1','Wrong Answer2','Correct Answer','Wrong Answer3'],
-        answer:2,
-        img: 'img/square.jpg'
+        id: 4,
+        q:'What is the sum of (15+13+11+9+7+5+3+1+0-2-4-6-8-10-12-14-16)/2',
+        options:['-4','0','2','8'],
+        answer:0,
     },
     {
-        q:'Example Question With Image2',
-        options:['Wrong Answer1','Wrong Answer2','Correct Answer','Wrong Answer3'],
-        answer:2,
-        img: 'img/square.jpg'
+        id: 5,
+        q:"If the line start at point A and line can't be overlapped. What are the point that line will end with most covered.",
+        options:['A','B','C','D','E','F'],
+        answer:5,
+        img: 'https://i.imgur.io/OD7QCmx_d.webp?maxwidth=640&shape=thumb&fidelity=medium'
     }
 ]
