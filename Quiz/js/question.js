4f6dcedfe717892e5b69e5469b7bb23408d28836
DevColaDavid <129636069+DevColaDavid@users.noreply.github.com>
2023-04-03 14:23:13 -1000

    Update question.js

--- Quiz/js/question.js
+++ Quiz/js/question.js
@@ -1,33 +1,63 @@
 const quiz = [
     {
-        id:1,
+        id: '1',
         q:'1x2x3x4x5x6x7x8x9x1x0x11x12x13x14x15x16x91',
         options:['0','1','2','3'],
         answer:0,
     },
     {
-        id:2,
+        id: '2',
         q:'What is the value of a if (6+1)x3x2+a=a^2? Write the answer as a integer.',
         options:['7','-2','12','0'],
         answer:0
     },
     {
-        id:3,
+        id: '3',
         q:'If someone added letters to make the word "David" with 5 letters into "Dwavido," with 7 letters, how much did the number letters increase, in percent?',
         options:['40','50','60','70'],
         answer:0,
     },
     {
-        id: 4,
+        id: '4',
         q:'What is the sum of (15+13+11+9+7+5+3+1+0-2-4-6-8-10-12-14-16)/2',
         options:['-4','0','2','8'],
         answer:0,
     },
     {
-        id: 5,
+        id: '5',
         q:"If the line start at point A and line can't be overlapped. What are the point that line will end with most covered.",
         options:['A','B','C','D','E','F'],
         answer:5,
         img: 'https://i.imgur.io/OD7QCmx_d.webp?maxwidth=640&shape=thumb&fidelity=medium'
+    },
+    {
+        id: '6',
+        q:"Kamehameha I ate 5 pancakes, Kamehameha II ate 7 Pancakes, Liliuokalani ate 3 pancakes. If Moana ate more than all of them combined because she was just cooler, what is the least amount of pancakes that Moana could have eaten?",
+        options:['16','20','28','8'],
+        answer:0,
+    },
+    {
+        id: '7',
+        q:"If 3 O is 5 A, 23 A is 8 H, and 10 H is 6 U, what is 69 O converted to U?",
+        options:['24','25','23','20'],
+        answer:0,
+    },
+    {
+        id: '8',
+        q:"What is sum of all possible value X? If 35/X = X+2",
+        options:['-2','1','4','-3'],
+        answer:0,
+    },
+    {
+        id: '9',
+        q:"What is x if 3(4+7x)+x+13=69?",
+        options:['2','1','-4','3'],
+        answer:0,
+    },
+    {
+        id: 'A',
+        q:"If 1/(2x-7)=2x-7. What is -4x?",
+        options:['14','24','4','34'],
+        answer:0,
     }
 ]
